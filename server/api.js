254006e0d931d7f61ba66d817ae81b5ed869cad9
Beau Dobbin <beau@beaunus.com>
2018-06-06 10:18:40 +0900

    Serve dummy data at API

--- server/api.js
+++ server/api.js
@@ -1,7 +1,16 @@
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
-  res.status(200).send("Hello world");
+  res.status(200).send({
+    "20180604": [
+      {
+        title: "The Headline",
+        text: "So and so did an interesting thing...",
+        link: "https://www.thing.com",
+        label: "New York Times"
+      }
+    ]
+  });
 });
 
 module.exports = router;
