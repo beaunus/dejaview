eaffad038104ce61bbeb8d333d9c3accbd382890
Beau Dobbin <beau@beaunus.com>
2018-06-06 15:01:17 +0900

    Add endpoint for date.

--- server/api.js
+++ server/api.js
@@ -1,16 +1,26 @@
 const router = require("express").Router();
+const db = require("knex")({
+  client: "pg",
+  connection: process.env.DATABASE_URL
+});
 
-router.get("/", (req, res) => {
-  res.status(200).send({
-    "20180604": [
-      {
-        title: "The Headline",
-        text: "So and so did an interesting thing...",
-        link: "https://www.thing.com",
-        label: "New York Times"
-      }
-    ]
-  });
+router.get("/:date", (req, res) => {
+  const year = req.params.date.slice(0, 4);
+  const month = req.params.date.slice(4, 6);
+  const date = req.params.date.slice(6, 8);
+  const targetDate = new Date(`${year}-${month}-${date}`);
+  const nextDate = new Date(targetDate.setDate(targetDate.getDate() + 1));
+  const weekBeforeDate = new Date(targetDate.setDate(targetDate.getDate() - 7));
+
+  const results = db("events")
+    .select()
+    .whereBetween("datetime", [weekBeforeDate, nextDate])
+    .then(result => {
+      res.status(200).send(result);
+    })
+    .catch(error => {
+      res.status(400).send("The date must be in YYYYMMDD format.");
+    });
 });
 
 module.exports = router;
