8bcef573fdddcccf3395c4c6ef7e3e4734e113c1
Mike Donnelly <sqldonnelly@gmail.com>
2018-06-07 12:06:31 +0900

    Show data from database by default

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,35 @@ import React, { Component } from "react";
 import "./styles/App.css";
 import Lifeline from "./components/Lifeline.jsx";
 import DatePicker from "./components/DatePicker.jsx";
-import data from "./data";
+import axios from "axios";
 import LabelFilter from "./components/LabelFilter";
+import moment from "moment";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      events: data
+      selectedDate: undefined,
+      events: {}
     };
     this.changeDate = this.changeDate.bind(this);
   }
 
+  async componentDidMount() {
+    const selectedDate = moment().format("YYYY-MM-DD");
+    const events = (await axios.get(`/api/v1/${selectedDate}`)).data;
+
+    this.setState({ selectedDate, events });
+  }
+
   changeDate(date) {
-    const strDate = date.replace(/-/g, "");
-    const nextDay = Number(strDate) - 1;
-    if (data[strDate] !== undefined && data[nextDay.toString()] !== undefined) {
-      const events = {};
-      events[strDate] = data[strDate];
-      events[nextDay.toString()] = data[nextDay.toString()];
-      this.setState({ events });
-    }
+    // const nextDay = Number(date) - 1;
+    // if (data[strDate] !== undefined && data[nextDay.toString()] !== undefined) {
+    //   const events = {};
+    //   events[date] = data[date];
+    //   events[nextDay.toString()] = data[nextDay.toString()];
+    //   this.setState({ selectedDate: date, events });
+    // }
   }
 
   render() {
