d68bc734f878398b8c6f816aea30677166968620
Mike Donnelly <sqldonnelly@gmail.com>
2018-06-12 01:07:06 +0900

    Add billboard logo

--- src/components/logoMap.js
+++ src/components/logoMap.js
@@ -1,6 +1,7 @@
 const logoMap = {
   "New York Times": "/images/nyt.png",
-  Wikipedia: "/images/wikipedia.svg"
+  Wikipedia: "/images/wikipedia.svg",
+  "Billboard Hot 100": "/images/billboard.jpg"
 };
 
 module.exports = logoMap;
