379c3f15f0c2de67dea820b5e3eab9d9f0416ed5
Beau Dobbin <beau@beaunus.com>
2018-06-12 14:07:15 +0900

    Adjust fonts and font sizes

--- src/components/Lifeline.jsx
+++ src/components/Lifeline.jsx
@@ -1,57 +1,65 @@
 import React from "react";
-import "../styles/Lifeline.css";
+import "../styles/New.css";
 import moment from "moment";
 import logoMap from "./logoMap";
 
 function Lifeline(props) {
   return (
     <div className="lifeline">
-      <ul>
-        {Object.keys(props.events)
-          .sort((a, b) => moment(new Date(b)) > moment(new Date(a)))
-          .map((key, index) => {
-            return (
-              <li key={"li_" + index}>
-                <div className="lifeline scale-in-center">
-                  <div key={index} className="pub-date">
-                    {moment(new Date(key)).format("MMMM D, YYYY")}
-                  </div>
-                  {Object.keys(props.events[key]).map((subKey, index2) => {
-                    return (
-                      <div className="headline" key={"div_" + index}>
+      {Object.keys(props.events)
+        .sort((a, b) => moment(new Date(b)) > moment(new Date(a)))
+        .map((key, index) => {
+          const classNames = ["container"];
+          if (index % 2 === 0) {
+            classNames.push("left");
+          } else {
+            classNames.push("right");
+          }
+          return (
+            <div key={key} className={classNames.join(" ")}>
+              <div className="content">
+                <div className="pub-date">
+                  {moment(new Date(key)).format("MMMM D, YYYY")}
+                </div>
+                {Object.keys(props.events[key]).map((subKey, index2) => {
+                  return (
+                    <div key={subKey}>
+                      <div className="headline">
                         <img
                           className="label-logo"
                           src={logoMap[subKey]}
-                          alt="New York Times Logo"
+                          alt={subKey + " Logo"}
                         />
-                        {props.events[key][subKey][0].link.length > 0 ? (
-                          <a
-                            href={props.events[key][subKey][0].link}
-                            key={index}
-                            target="_blank"
-                          >
-                            {props.events[key][subKey][0].title}
-                          </a>
-                        ) : (
-                          `${props.events[key][subKey][0].title}`
-                        )}
-                        <p
-                          className="event-text"
-                          dangerouslySetInnerHTML={{
-                            __html: props.events[key][subKey][0].text.replace(
-                              "li>",
-                              "span>"
-                            )
-                          }}
-                        />
+                        <div>
+                          {props.events[key][subKey][0].link.length > 0 ? (
+                            <a
+                              href={props.events[key][subKey][0].link}
+                              key={index}
+                              target="_blank"
+                            >
+                              {props.events[key][subKey][0].title}
+                            </a>
+                          ) : (
+                            `${props.events[key][subKey][0].title}`
+                          )}
+                        </div>
                       </div>
-                    );
-                  })}
-                </div>
-              </li>
-            );
-          })}
-      </ul>
+                      <p
+                        className="event-text"
+                        dangerouslySetInnerHTML={{
+                          __html: props.events[key][subKey][0].text.replace(
+                            "li>",
+                            "span>"
+                          )
+                        }}
+                      />
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          );
+        })}
     </div>
   );
 }
