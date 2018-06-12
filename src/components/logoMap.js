99a6631cc512979d405aedff346af0fd158fd285
Mike Donnelly <sqldonnelly@gmail.com>
2018-06-12 13:32:40 +0900

    Change Billboard label

--- src/components/logoMap.js
+++ src/components/logoMap.js
@@ -1,7 +1,7 @@
 const logoMap = {
   "New York Times": "/images/nyt.png",
   Wikipedia: "/images/wikipedia.svg",
-  "Billboard Hot 100": "/images/billboard.jpg"
+  Billboard: "/images/billboard.jpg"
 };
 
 module.exports = logoMap;
