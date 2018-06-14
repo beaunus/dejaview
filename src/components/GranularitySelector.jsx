8dc14d2685e5b4e33167137831f6b30fe922e1b3
Beau Dobbin <beau@beaunus.com>
2018-06-14 12:12:25 +0900

    Fix radio buttons to call event handler

--- src/components/GranularitySelector.jsx
+++ src/components/GranularitySelector.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 import "../styles/GranularitySelector.css";
 
+/**
+ * Select granularity according to the click or key press event on corresponding button.
+ * @param {Event} event
+ */
+const selectGranularity = event => {
+  event.preventDefault();
+  event.target.parentNode.querySelector("input").checked = true;
+};
+
 function GranularitySelector(props) {
   return (
     <div id="granularity-selector">
@@ -8,38 +17,59 @@ function GranularitySelector(props) {
         <input
           type="radio"
           name="granularity"
+          id="granularity"
           value="day"
-          id="granularityDay"
           defaultChecked="checked"
         />
-        <div className="label">Day</div>
+        <div
+          className="label"
+          id="granularityDay"
+          tabIndex="0"
+          role="button"
+          onClick={selectGranularity}
+          onKeyPress={selectGranularity}
+        >
+          Day
+        </div>
       </label>
       <label htmlFor="granularityWeek">
-        <input
-          type="radio"
-          name="granularity"
-          value="week"
+        <input type="radio" name="granularity" id="granularity" value="week" />
+        <div
+          className="label"
           id="granularityWeek"
-        />
-        <div className="label">Week</div>
+          tabIndex="0"
+          role="button"
+          onClick={selectGranularity}
+          onKeyPress={selectGranularity}
+        >
+          Week
+        </div>
       </label>
       <label htmlFor="granularityMonth">
-        <input
-          type="radio"
-          name="granularity"
-          value="month"
+        <input type="radio" name="granularity" id="granularity" value="month" />
+        <div
+          className="label"
           id="granularityMonth"
-        />
-        <div className="label">Month</div>
+          tabIndex="0"
+          role="button"
+          onClick={selectGranularity}
+          onKeyPress={selectGranularity}
+        >
+          Month
+        </div>
       </label>
       <label htmlFor="granularityYear">
-        <input
-          type="radio"
-          name="granularity"
-          value="year"
+        <input type="radio" name="granularity" id="granularity" value="year" />
+        <div
+          className="label"
           id="granularityYear"
-        />
-        <div className="label">Year</div>
+          tabIndex="0"
+          role="button"
+          onClick={selectGranularity}
+          onKeyPress={selectGranularity}
+        >
+          Year
+        </div>
       </label>
     </div>
   );
