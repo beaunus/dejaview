d62130f324ad92aed8b3af9e1c6ba2a4c2328f2c
Beau Dobbin <beau@beaunus.com>
2018-06-14 21:41:32 +0900

    Encapsulate Cards and Events

--- src/components/Lifeline.jsx
+++ src/components/Lifeline.jsx
@@ -4,12 +4,67 @@ import moment from "moment";
 import logoMap from "./logoMap";
 import { toggleHidden } from "../scripts/Lifeline.js";
 
+function Event(props) {
+  console.log("props.event", props.event);
+  return (
+    <div key={props.label} className="event">
+      <div className="headline">
+        <img
+          className="label-logo"
+          src={logoMap[props.label]}
+          alt={`${props.label} Logo`}
+        />
+        <div>
+          {props.event.link.length > 0 ? (
+            <a
+              href={props.event.link}
+              key={props.index}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {props.event.title}
+            </a>
+          ) : (
+            `${props.event.title}`
+          )}
+        </div>
+        <input type="checkbox" onClick={toggleHidden} />
+        <div className="more">more</div>
+      </div>
+      <p
+        className="event-text hidden"
+        dangerouslySetInnerHTML={{
+          __html: props.event.text.replace("li>", "span>")
+        }}
+      />
+    </div>
+  );
+}
+
+function Card(props) {
+  return (
+    <div className={props.classNames.join(" ")}>
+      <div className="content">
+        <div className="pub-date">
+          {moment(new Date(props.date)).format("MMMM D, YYYY")}
+        </div>
+        {Object.keys(props.events).map((label, index) => {
+          return props.events[label].map((event, eventIndex) => {
+            return <Event key={eventIndex} label={label} event={event} />;
+          });
+        })}
+      </div>
+    </div>
+  );
+}
+
 function Lifeline(props) {
+  console.log(props.events);
   return (
     <div className="lifeline">
       {Object.keys(props.events)
         .sort((a, b) => moment(new Date(b)) > moment(new Date(a)))
-        .map((key, index) => {
+        .map((date, index) => {
           const classNames = ["container"];
           if (index % 2 === 0) {
             classNames.push("left");
@@ -17,50 +72,13 @@ function Lifeline(props) {
             classNames.push("right");
           }
           return (
-            <div key={key} className={classNames.join(" ")}>
-              <div className="content">
-                <div className="pub-date">
-                  {moment(new Date(key)).format("MMMM D, YYYY")}
-                </div>
-                {Object.keys(props.events[key]).map((subKey, index2) => {
-                  return (
-                    <div key={subKey} className="event">
-                      <div className="headline">
-                        <img
-                          className="label-logo"
-                          src={logoMap[subKey]}
-                          alt={subKey + " Logo"}
-                        />
-                        <div>
-                          {props.events[key][subKey][0].link.length > 0 ? (
-                            <a
-                              href={props.events[key][subKey][0].link}
-                              key={index}
-                              target="_blank"
-                            >
-                              {props.events[key][subKey][0].title}
-                            </a>
-                          ) : (
-                            `${props.events[key][subKey][0].title}`
-                          )}
-                        </div>
-                        <input type="checkbox" onClick={toggleHidden} />
-                        <div className="more">more</div>
-                      </div>
-                      <p
-                        className="event-text hidden"
-                        dangerouslySetInnerHTML={{
-                          __html: props.events[key][subKey][0].text.replace(
-                            "li>",
-                            "span>"
-                          )
-                        }}
-                      />
-                    </div>
-                  );
-                })}
-              </div>
-            </div>
+            <Card
+              events={props.events[date]}
+              date={date}
+              key={index}
+              classNames={classNames}
+              index={index}
+            />
           );
         })}
     </div>
