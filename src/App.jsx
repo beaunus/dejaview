3bb94bd2651e8ade88220b94b69f8852cb6f2aa0
Beau Dobbin <beau@beaunus.com>
2018-06-18 18:03:11 +0900

    Change Lifeline to Deja View

--- src/App.jsx
+++ src/App.jsx
@@ -90,7 +90,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="header">Lifeline</div>
+        <div className="header">Déjà View</div>
         <Granularity
           granularity={this.state.granularity}
           changeGranularity={this.changeGranularity}
