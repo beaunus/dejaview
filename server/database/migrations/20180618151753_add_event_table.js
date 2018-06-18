e1f797d5c9d4ed2e9668b463a8442ff2107480d0
Mike Donnelly <sqldonnelly@gmail.com>
2018-06-18 15:32:35 +0900

    Add Knex migrations for db tables

---
+++ server/database/migrations/20180618151753_add_event_table.js
@@ -0,0 +1,24 @@
+exports.up = function(knex) {
+  return knex.schema.hasTable("event").then(exists => {
+    if (!exists) {
+      return knex.schema.createTable("event", table => {
+        table.increments().primary();
+        table.timestamp("timestamp", true).notNull();
+        table.text("title").notNull();
+        table.text("text");
+        table.text("link");
+        table.text("image_link");
+        table.text("media_link");
+        table.integer("label_id");
+        table
+          .foreign("label_id")
+          .references("id")
+          .inTable("label");
+      });
+    }
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable("event");
+};
