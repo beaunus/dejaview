4d692e05c7cf477066e9050af153421ebdc2ef65
Beau Dobbin <beau@beaunus.com>
2018-06-24 21:40:00 +0900

    Return Lifeline to dumb component

--- src/components/Lifeline.jsx
+++ src/components/Lifeline.jsx
@@ -4,43 +4,31 @@ import Card from "./Card";
 import InfiniteScroll from "react-infinite-scroller";
 import "../styles/Lifeline.css";
 
-class Lifeline extends React.Component {
-  constructor(props) {
-    super(props);
-    this.api = {
-      baseUrl: "/api/v1",
-      queryParams: `?granularity=${props.granularity}s&num=7`
-    };
-  }
-
-  render() {
-    return (
-      <div className="lifeline">
-        <InfiniteScroll
-          pageStart={0}
-          loadMore={this.props.loadMoreEvents}
-          hasMore={true || false}
-          loader={
-            <div className="loader" key={0}>
-              Loading ...
-            </div>
-          }
-        >
-          {Object.keys(this.props.events).map((date, index) => (
-            <Card
-              date={date}
-              events={this.props.events[date]}
-              granularity={this.props.granularity}
-              isEven={index % 2 === 0}
-              key={index}
-              labels={this.props.labels}
-            />
-          ))}
-        </InfiniteScroll>
-      </div>
-    );
-  }
-}
+const Lifeline = props => (
+  <div className="lifeline">
+    <InfiniteScroll
+      pageStart={0}
+      loadMore={props.loadMoreEvents}
+      hasMore={true || false}
+      loader={
+        <div className="loader" key={0}>
+          Loading ...
+        </div>
+      }
+    >
+      {Object.keys(props.events).map((date, index) => (
+        <Card
+          date={date}
+          events={props.events[date]}
+          granularity={props.granularity}
+          isEven={index % 2 === 0}
+          key={index}
+          labels={props.labels}
+        />
+      ))}
+    </InfiniteScroll>
+  </div>
+);
 
 Lifeline.propTypes = {
   events: PropTypes.object.isRequired,
