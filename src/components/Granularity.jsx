94fd3e5edfe7c9db8f4bd74eff9cddb790e053c6
Beau Dobbin <beau@beaunus.com>
2018-06-27 12:21:09 +0900

    Remove Today button and align header vertically

--- src/components/Granularity.jsx
+++ src/components/Granularity.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 import GranularityButton from "./GranularityButton";
-import moment from "moment";
 import "../styles/Granularity.css";
 
 const choices = ["year", "month", "week", "day"];
@@ -18,12 +17,6 @@ const Granularity = props => (
         />
       ))}
     </div>
-    <button
-      id="btn-today"
-      onClick={() => props.changeDate(moment().format("YYYY-MM-DD"))}
-    >
-      Today
-    </button>
   </div>
 );
 
